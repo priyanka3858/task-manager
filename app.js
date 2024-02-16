31b1c3a887ea4db3d1fe8cfd5cc0e977eee49665
priyanka <priyanka3858@gmail.com>
2024-02-16 16:51:28 -0500

    cors

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(express.json());
 app.use(express.static("./public"));
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+  next();
+});
+
 app.use("/api/v1/tasks", tasks);
 
 // get all task
